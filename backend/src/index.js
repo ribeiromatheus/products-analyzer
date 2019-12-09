e21365e34906d3ad205a9078056de05e1405496e
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-12-09 16:18:07 -0300

    Refactoring

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,32 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const socketio = require('socket.io');
+const http = require('http');
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    connectedUsers[user] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 app.use(cors());
@@ -19,4 +38,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
+server.listen(3333);
