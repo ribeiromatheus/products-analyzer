ad0ae97a1151b558e662c03973a994025f149a4f
Matheus Ribeiro <41703972+ribeiromatheus@users.noreply.github.com>
2019-12-23 16:41:55 -0300

    feat: add image url path

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
