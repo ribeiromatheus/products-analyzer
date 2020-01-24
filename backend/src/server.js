f0022775e8e0d9d0c20dca6c9dff59a71764ff71
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-01-24 16:04:51 -0300

    feat: config socket io

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,13 +5,34 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
 
+const server = http.Server(app);
+const io = socketio(server);
+
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 app.use(cors());
@@ -21,4 +42,4 @@ app.use(morgan('dev'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
