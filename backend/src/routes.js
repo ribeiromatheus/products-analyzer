20741484471162b65762123d3f9c89e26bb9087f
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-01-31 14:53:32 -0300

    style:  libs and path files separted by break line

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const multer = require('multer');
+
 const multerConfig = require('./config/multer');
 const VisualRecognitionController = require('./controller/VisualRecognitionController');
 const UserController = require('./controller/UserController');
 
 const routes = express.Router();
 
-routes.post('/login', UserController.login);
 routes.get('/recognize', VisualRecognitionController.index);
+routes.post('/login', UserController.login);
 routes.post('/recognize', multer(multerConfig).single('file'), VisualRecognitionController.storeAndRecognize);
 routes.delete('/recognize/:id', VisualRecognitionController.remove);
 
