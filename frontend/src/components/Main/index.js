dfc8bddca3d00988d8cae15d20e1505413f181c8
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-01-31 16:01:47 -0300

    feat: get products with socket.io

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,91 +1,36 @@
-import React, { Component } from 'react';
-import api from '../../services/api';
-import { uniqueId } from 'lodash';
-import filesize from 'filesize';
+import React, { useState, useEffect } from 'react';
+import socketio from 'socket.io-client';
+
 import GlobalStyle from '../../styles/global';
 import { Container, Content } from './styles';
-import Upload from '../Upload';
+
 import FileList from '../FileList';
 import Logo from '../Logo';
 
-export default class Main extends Component {
-    state = {
-        uploadedFiles: []
-    };
+export default function Main() {
+    const [products, setProducts] = useState([]);
 
-    handleUpload = files => {
-        const uploadedFiles = files.map(file => ({
-            file,
-            id: uniqueId(),
-            name: file.name,
-            readableSize: filesize(file.size),
-            preview: URL.createObjectURL(file),
-            progress: 0,
-            status: '',
-            uploaded: false,
-            error: false
-        }));
+    const user_id = localStorage.getItem('user');
 
-        this.setState({
-            uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    const socket = socketio('http://localhost:3333', {
+        query: { user_id }
+    });
+
+    useEffect(() => {
+        socket.on('product', data => {
+            setProducts([...products, data]);
         });
+    }, [products, socket]);
 
-        uploadedFiles.forEach(this.processUpload);
-    };
-
-    updateFile = (id, data) => {
-        this.setState({
-            uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
-                return id === uploadedFile.id
-                    ? { ...uploadedFile, ...data }
-                    : uploadedFile;
-            })
-        });
-    };
-
-    processUpload = uploadedFile => {
-        const data = new FormData();
-        const user_id = localStorage.getItem('user');
-
-        data.append('file', uploadedFile.file, uploadedFile.name);
-
-        api.post('recognize', data, {
-            headers: { user_id },
-            onUploadProgress: e => {
-                const progress = parseInt(Math.round((e.loaded * 100) / e.total));
-
-                this.updateFile(uploadedFile.id, {
-                    progress
-                });
-            }
-        })
-            .then(response => {
-                this.updateFile(uploadedFile.id, {
-                    uploaded: true,
-                    status: response.data.status,
-                });
-            })
-            .catch(() => {
-                this.updateFile(uploadedFile.id, {
-                    error: true
-                });
-            });
-    };
-
-    render() {
-        const { uploadedFiles } = this.state;
-
-        return (
-            <Container>
-                <Content>
-                    <Logo />
-                    <Upload onUpload={this.handleUpload} />
-                    {!!uploadedFiles.length && (
-                        <FileList files={uploadedFiles} />
-                    )}
-                </Content>
-                <GlobalStyle />
-            </Container>
-        );
-    }
+    return (
+        <Container>
+            <Content>
+                <Logo />
+                {!!products.length && (
+                    <FileList files={products} />
+                )}
+            </Content>
+            <GlobalStyle />
+        </Container>
+    );
 }
