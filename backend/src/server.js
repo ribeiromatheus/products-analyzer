1fd1d7aac3fc169a7e9fb588c291aa63389e36c3
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-01-31 14:53:08 -0300

    style: remove morgan module line

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
@@ -38,7 +37,6 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
 app.use(routes);
 
